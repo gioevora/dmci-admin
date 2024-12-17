66892d51ec39a704907de93e3269bed7875576c5
unknown <giolo.evora@gmail.com>
2024-12-17 11:33:37 +0800

    first commit

---
+++ app/property/page.tsx
@@ -0,0 +1,9 @@
+import { title } from "@/components/primitives";
+
+export default function PropertyPage() {
+  return (
+    <div>
+      <h1 className={title()}>this is the property page</h1>
+    </div>
+  );
+}
