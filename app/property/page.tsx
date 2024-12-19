e3f45da942c860ecc68bafca698226f4b7de0f4a
tnmtthw <118366103+tnmtthw@users.noreply.github.com>
2024-12-19 08:29:04 +0800

    Delete template page

--- app/property/page.tsx
+++
@@ -1,9 +0,0 @@
-import { title } from "@/components/primitives";
-
-export default function PropertyPage() {
-  return (
-    <div>
-      <h1 className={title()}>this is the property page</h1>
-    </div>
-  );
-}
