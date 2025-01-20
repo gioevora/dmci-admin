3872f26405ad4caf34cdd11107bfb5054d87d523
tnmtthw <118366103+tnmtthw@users.noreply.github.com>
2025-01-20 09:21:18 +0800

    Install hot-toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,12 @@ import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
 import { Link } from "@nextui-org/link";
 import clsx from "clsx";
+import { Toaster } from "react-hot-toast";
 
 import { Providers } from "@/app/providers";
 
 import { fontSans } from "@/config/fonts";
 import Navbar from "@/components/navbar";
-
 
 export const metadata: Metadata = {
   title: "ADMIN",
@@ -31,14 +31,23 @@ export default function RootLayout({
       <body
         className={clsx(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable,
+          fontSans.variable
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-
           <div className="relative flex flex-col h-screen">
             <Navbar />
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+              {/* Toaster with Dark Theme Support */}
+              <Toaster
+                toastOptions={{
+                  className: "bg-background text-default-500 dark:bg-gray-800 dark:text-white",
+                  style: {
+                    borderRadius: "8px",
+                    padding: "12px",
+                  },
+                }}
+              />
               {children}
             </main>
             <footer className="w-full flex items-center justify-center py-3">
