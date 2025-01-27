d1afc561cf821db3f2248421229b65069092afef
tnmtthw <118366103+tnmtthw@users.noreply.github.com>
2025-01-27 11:40:34 +0800

    comment submission table

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -136,10 +136,10 @@ const Navbar = () => {
                 <Calendar className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
                 <span className="ms-3">Schedules</span>
               </Link>
-              <Link href="/admin/submissions" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-[#27272a] group">
+              {/* <Link href="/admin/submissions" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-[#27272a] group">
                 <Building className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
                 <span className="ms-3">Submissions</span>
-              </Link>
+              </Link> */}
               <Link href="/admin/testimonials" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-[#27272a] group">
                 <Star className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
                 <span className="ms-3">Testimonials</span>
